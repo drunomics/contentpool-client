20702f6ad8b8e67a6324d6cd6630f3632fa1044f
fago <fago@wolfgangziegler.net>
2018-08-17 15:31:57 +0200

    Remove drush debug command.

--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -16,5 +16,4 @@ docker-compose exec web drush en contentpool_client -y
 
 # Run auto-setup with the default contentpool pass,
 # see drunomics/contentpool:scripts/init-project.sh
-docker-compose exec web drush
 docker-compose exec web drush contentpool-client:setup http://replicator:changeme@contentpool-project.localdev.space/relaxed
