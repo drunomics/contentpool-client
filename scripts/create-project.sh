c69ca3fda53b04b04b88fee0f3f655645c8ae4fe
fago <fago@wolfgangziegler.net>
2018-08-20 17:45:45 +0200

    Allow cross-project testing of changes via central environment variable overrides.

--- scripts/create-project.sh
+++ scripts/create-project.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-PHAPP_VERSION=0.6.7
-
-set -e
-set -x
+set -ex
 cd `dirname $0`/..
 
-if ! command -v phapp > /dev/null; then
-  echo Installing phapp...
-  curl -L https://github.com/drunomics/phapp-cli/releases/download/$PHAPP_VERSION/phapp.phar > phapp
-  chmod +x phapp
-  sudo mv phapp /usr/local/bin/phapp
-else
-  echo Phapp version `phapp --version` found.
-fi
+source ./scripts/util/per-branch-env.sh
+./scripts/util/install-phapp.sh
 
 [ ! -d ../satellite-project ] || (echo "Old project is still existing, please remove ../satellite-project." && exit 1)
 
@@ -41,9 +32,9 @@ END
 echo "Setting up project..."
 phapp setup localdev
 
-if [[ -f ../$MODULE_DIR/scripts/per-branch-pre-build-hook/${GIT_BRANCH/\//--}.sh ]]; then
-  echo "Executing pre-build hook for branch $GIT_BRANCH"
-  ../$MODULE_DIR/scripts/per-branch-pre-build-hook/${GIT_BRANCH/\//--}.sh
+if [[ ! -z "$PRE_BUILD_COMMANDS" ]]; then
+  echo "Executing pre-build commands for branch $GIT_BRANCH"
+  eval "$PRE_BUILD_COMMANDS"
 fi
 
 # Run build on the host so we can leverage build caches.
