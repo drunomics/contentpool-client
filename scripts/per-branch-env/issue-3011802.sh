5d0a172d4543c966ab820d2ace0488ff5535bf55
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2019-01-15 16:15:08 +0100

    Make it testable.

---
+++ scripts/per-branch-env/issue-3011802.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -ex
+
+export LAUNCH_CONTENTPOOL_GIT_BRANCH="issue-3011802"
+export PRE_BUILD_COMMANDS='composer require drunomics/contentpool_replication:"dev-issue-3011802 as 1.1.0"'
