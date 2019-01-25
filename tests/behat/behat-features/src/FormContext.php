7851d24ac39487fa8098e173e7dbc52554f51eeb
Alen Pagac <alen.pagac@drunomics.com>
2019-01-25 21:05:32 +0100

    WV-2495: Remove unused methods.

--- tests/behat/behat-features/src/FormContext.php
+++ tests/behat/behat-features/src/FormContext.php
@@ -9,34 +9,6 @@ use Behat\Mink\Exception\ExpectationException;
 class FormContext extends RawDrupalContext {
 
   /**
-   * @Then :element_selector value should be :element_value
-   */
-  public function shouldBe($element_selector, $element_value) {
-    $result = $this->getSession()
-      ->evaluateScript("jQuery(\"$element_selector\").val()");
-    if (!$result) {
-      throw new ExpectationException('Value is empty.', $this->getSession());
-    }
-    elseif ($result !== $element_value) {
-      throw new ExpectationException(sprintf("Expected value: %s, got: %s", $element_value, $result), $this->getSession());
-    }
-  }
-
-  /**
-   * @Then :element_selector value should not be :element_value
-   */
-  public function shouldNotBe($element_selector, $element_value) {
-    $result = $this->getSession()
-      ->evaluateScript("jQuery(\"$element_selector\").val()");
-    if (!$result) {
-      throw new ExpectationException('Value is empty.', $this->getSession());
-    }
-    elseif ($result == $element_value) {
-      throw new ExpectationException(sprintf("Expected value: %s, got: %s", $element_value, $result), $this->getSession());
-    }
-  }
-
-  /**
    * @Then :element_selector value is empty
    */
   public function shouldBeEmpty($element_selector) {
