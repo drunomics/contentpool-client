5bf30d4e27b44e4c39963b2e4f63ba7784755fd7
Alen Pagač <alen.pagac@drunomics.com>
2019-01-30 14:57:04 +0100

    WV-2495: Pre build command branch name.

--- scripts/per-branch-env/feature--WV-2495.sh
+++ scripts/per-branch-env/feature--WV-2495.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-export PRE_BUILD_COMMANDS="composer require drunomics/behat-drupal-utils:'dev-feature/WV-2495 as 2.3.0' --no-update"
+export PRE_BUILD_COMMANDS="composer require drunomics/behat-drupal-utils:'dev-feature/WV-2495 as 2.2.0' --no-update"
