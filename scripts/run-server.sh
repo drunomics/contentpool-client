c436045e3acf282ab35a9b9a6c0292a53791e5d1
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2019-05-16 16:13:16 +0200

    DEV-1489: Remove custom network aliases.

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -8,9 +8,4 @@ cd ../satellite-project/
 
 echo "Running server..."
 docker-compose up -d --build
-
-# Allow access the contentpool via the traefik network.
-docker network connect --alias satellite-project_cli_1 traefik satellite-project_cli_1
-docker network connect --alias satellite-project_php_1 traefik satellite-project_php_1
-
 echo "Waiting for mysql to come up..." && docker-compose exec cli /bin/bash -c "while ! echo exit | nc mariadb 3306; do sleep 1; done" >/dev/null
