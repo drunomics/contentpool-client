593d41210900a0bd708a19bc1625f988342cd184
Volodymyr Mostepaniuk <mostepaniukvm@gmail.com>
2020-05-26 13:01:15 +0300

    WV-3831: Corrected per-branch script.

--- scripts/per-branch-env/feature--WV-3831.sh
+++ scripts/per-branch-env/feature--WV-3831.sh
@@ -2,4 +2,3 @@
 set -ex
 
 export LAUNCH_CONTENTPOOL_GIT_BRANCH="feature/WV-3831"
-export PRE_BUILD_COMMANDS="composer require drunomics/contentpool_data_model:'dev-feature/WV-3831 as 1.1.2' --no-update"
